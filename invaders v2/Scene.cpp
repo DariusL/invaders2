ba3aff821a7e4b7b51aaa6769329ab8faabe77bb
dariusl <d.lapunas@gmail.com>
2013-11-15 20:13:42 +0200

    gsdagsd

--- invaders v2/Scene.cpp
+++ invaders v2/Scene.cpp
@@ -6,28 +6,9 @@
 #include "WaterShader.h"
 
 Scene::Scene(void)
-:gabenizer(ZeroVec3, ZeroVec3, RM::Get().GetTexturedModel(RM::TexturedModels::INV_BOX), RM::Get().GetShader<TextureShader>(),
-RM::Get().GetTexture(RM::Textures::TEXTURE_GABEN), XMFLOAT3(400.0f, 400.0f, 400.0f)),
-
-light(XMFLOAT3(0.0f, 0.0f, -30.0f), XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), RM::Get().GetModel(RM::Models::MODEL_BALL), RM::Get().GetShader<ColorShader>()),
-
-bumpy(XMFLOAT3(0.0f, 0.0f, -25.0f), RM::Get().GetNormalMappedModel(), RM::Get().GetShader<NormalMappedShader>(),
-RM::Get().GetTexture(RM::Textures::TEXTURE_PAPER_NORMAL_MAP)),
-
-water(XMFLOAT3(0.0f, -15.0f, 0.0f), XMFLOAT3(XM_PIDIV2, 0.0f, 0.0f), RM::Get().GetTexturedModel(RM::TexturedModels::PLANE), 
-RM::Get().GetShader<WaterShader>(), 400, 400, 40.0f, 40.0f, RM::Get().GetTexture(RM::Textures::TEXTURE_WATER_NORMAL_MAP)),
-
-bath(XMFLOAT3(0.0f, -20.0f, 0.0f), ZeroVec3, RM::Get().GetTexturedModel(RM::TexturedModels::BATH), 
-RM::Get().GetShader<TextureShader>(), RM::Get().GetTexture(RM::Textures::WALL)),
-
-thing(XMFLOAT3(0.0f, -5.0f, -28.0f), RM::Get().GetNormalModel(), RM::Get().GetShader<PointSpecularShader>())
 {
 	camera.Move(0.0f, -10.0f, -50.0f);
-	started = false;
-	cameras.emplace_back(XMFLOAT3(0.0f, 0.0f, -50.0f), ZeroVec3, XMFLOAT3(0.0f, -5.0f, 0.0f), ZeroVec3,
-		RM::Get().GetTexturedModel(RM::TexturedModels::PLANE), RM::Get().GetShader<TextureShader>(), 400, 200, 20.0f, 10.0f);
-	mirrors.emplace_back(XMFLOAT3(50.0f, 0.0f, 0.0f), XMFLOAT3(0.0f, XM_PIDIV2, 0.0f), RM::Get().GetTexturedModel(RM::TexturedModels::PLANE),
-		RM::Get().GetShader<MirrorShader>(), 800, 800, 20.0f, 20.0f);
+	
 }
 
 
@@ -92,23 +73,10 @@ int Scene::OnLoop(int input, float frameLength)
 
 void Scene::Init(ComPtr<ID3D11Device> device)
 {
-	light.Init(device);
-	bumpy.Init(device);
-	for (auto &mirror : mirrors)
-		mirror.Init(device);
-	for (auto &camera : cameras)
-		camera.Init(device);
-	gabenizer.Init(device);
-	water.Init(device);
-	bath.Init(device);
-	thing.Init(device);
+	
 }
 
 void Scene::Render(const RenderParams &params)
 {
-	light.Render(params);
-	bumpy.Render(params);
-	gabenizer.Render(params);
-	bath.Render(params);
-	thing.Render(params);
+	
 }
--- invaders v2/Scene.h
+++ invaders v2/Scene.h
@@ -1,24 +1,11 @@
 #pragma once
 #include "iworld.h"
-#include "DrawableShooter.h"
-#include "DrawableTexturedEntity.h"
 #include "Globals.h"
 #include "Light.h"
-#include "DrawableBumpyEntity.h"
-#include "WaterPlane.h"
-#include "RemoteCamera.h"
-#include "Mirror.h"
-#include "WaterPlane.h"
+
 class Scene : public IWorld
 {
-	DrawableBumpyEntity bumpy;
-	Light light;
-	vector<RemoteCamera> cameras;
-	vector<SimpleMirror> mirrors;
-	SimpleTexturedEntity gabenizer;
-	SimpleWaterPlane water;
-	SimpleTexturedEntity bath;
-	DrawableEntity<NormalVertexType, PointSpecularShader> thing;
+	
 public:
 	Scene();
 	~Scene();
@@ -27,10 +14,7 @@ public:
 	void Stop();
 
 	int OnLoop(int input, float frameLength);
-	Light &GetLight(){ return light; }
-	vector<SimpleMirror> &GetMirrors(){ return mirrors; }
-	vector<RemoteCamera> &GetRemoteCameras(){ return cameras; }
-	SimpleWaterPlane &GetWater(){ return water; }
+	
 	void Init(ComPtr<ID3D11Device> device);
 	void Render(const RenderParams &params);
 };
