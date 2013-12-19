192bcffa10426e001964d3ab2a7a0f7693d63f62
dariusl <d.lapunas@gmail.com>
2013-12-19 12:55:25 +0200

    Added Texture

---
+++ invaders v2/Texture.cpp
@@ -0,0 +1,59 @@
+#include "includes.h"
+#include "Texture.h"
+#include "Utils.h"
+
+void Texture::Init(ComPtr<ID3D11Device> device)
+{
+	D3D11_TEXTURE2D_DESC texDesc;
+	uint bind = 0;
+	if (hasSRV)
+		bind |= D3D11_BIND_SHADER_RESOURCE;
+	if (hasUAV)
+		bind |= D3D11_BIND_UNORDERED_ACCESS;
+	if (hasRTV)
+		bind |= D3D11_BIND_RENDER_TARGET;
+	texDesc.ArraySize = 1;
+	texDesc.BindFlags = bind;
+	texDesc.CPUAccessFlags = 0;
+	texDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	texDesc.Height = height;
+	texDesc.Width = width;
+	texDesc.MipLevels = 1;
+	texDesc.MiscFlags = 0;
+	texDesc.SampleDesc.Count = 1;
+	texDesc.SampleDesc.Quality = 0;
+	texDesc.Usage = D3D11_USAGE_DEFAULT;
+
+	Assert(device->CreateTexture2D(&texDesc, NULL, &texture));
+
+	if (hasSRV)
+	{
+		D3D11_SHADER_RESOURCE_VIEW_DESC srvdesc;
+		srvdesc.Format = texDesc.Format;
+		srvdesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+		srvdesc.Texture2D.MipLevels = 1;
+		srvdesc.Texture2D.MostDetailedMip = 0;
+
+		Assert(device->CreateShaderResourceView(texture.Get(), &srvdesc, &shaderResourceView));
+	}
+
+	if (hasUAV)
+	{
+		D3D11_UNORDERED_ACCESS_VIEW_DESC uavDesc;
+		uavDesc.Format = texDesc.Format;
+		uavDesc.ViewDimension = D3D11_UAV_DIMENSION_TEXTURE2D;
+		uavDesc.Texture2D.MipSlice = 0;
+
+		Assert(device->CreateUnorderedAccessView(texture.Get(), &uavDesc, &unorderedAccessView));
+	}
+
+	if (hasRTV)
+	{
+		D3D11_RENDER_TARGET_VIEW_DESC rtvDesc;
+		rtvDesc.Format = texDesc.Format;
+		rtvDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+		rtvDesc.Texture2D.MipSlice = 0;
+
+		Assert(device->CreateRenderTargetView(texture.Get(), &rtvDesc, &renderTargetView));
+	}
+}
---
+++ invaders v2/Texture.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "includes.h"
+#include "Globals.h"
+using namespace Microsoft::WRL;
+class Texture
+{
+	ComPtr<ID3D11Texture2D> texture;
+	ComPtr<ID3D11ShaderResourceView> shaderResourceView;
+	ComPtr<ID3D11UnorderedAccessView> unorderedAccessView;
+	ComPtr<ID3D11RenderTargetView> renderTargetView;
+
+	bool hasSRV, hasUAV, hasRTV;
+	uint width, height;
+public:
+	Texture(uint width, uint height, bool createSRV = true, bool createRTV = true, bool createUAV = false) 
+		:hasSRV(createSRV), hasUAV(createUAV), hasRTV(createRTV), width(width), height(height){}
+	~Texture(){}
+
+	void Init(ComPtr<ID3D11Device> device);
+
+	ComPtr<ID3D11ShaderResourceView> GetSRV(){ return shaderResourceView; }
+	ComPtr<ID3D11UnorderedAccessView> GetUAV(){ return unorderedAccessView; }
+	ComPtr<ID3D11RenderTargetView> GetRTV(){ return renderTargetView; }
+};
