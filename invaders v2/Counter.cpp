c6af565ab32e063112df4d6078d18ae1a639ba0c
dariusl <d.lapunas@gmail.com>
2014-06-03 23:57:00 +0300

    Fix upgrade stuff

--- invaders v2/Counter.cpp
+++ invaders v2/Counter.cpp
@@ -4,24 +4,8 @@
 #include "ResourceManager.h"
 
 Counter::Counter(e::XMVECTOR pos, e::string text, float width, int initial, float scale)
-:left(Input::ACTION_LEFT), right(Input::ACTION_RIGHT),
-count(initial), Attribute(pos, text, width, e::to_string(initial))
+	:count(initial), Attribute(pos, text, width, e::to_string(initial))
 {
-}
-
-void Counter::Delay()
-{
-	left.Reset();
-	right.Reset();
-}
-
-bool Counter::Loop(InputType input)
-{
-	if (left.Register(input))
-		operator--();
-	if (right.Register(input))
-		operator++();
-	return left.IsDown(input) || right.IsDown(input);
 }
 
 Counter &Counter::operator+=(int off)
--- invaders v2/Counter.h
+++ invaders v2/Counter.h
@@ -5,12 +5,9 @@
 class Counter : public Attribute
 {
 	int count;
-	PressRegister left, right;
 public:
 	Counter(e::XMVECTOR pos, e::string text, float width, int initial = 0, float scale = 1.0f);
 	void SetCount(int count);
-	bool Loop(InputType input);
-	void Delay();
 	int GetValue(){ return count; }
 	Counter &operator+=(int off);
 	Counter &operator-=(int off);
