dd949de23503896b9099ab2fc9d8a54a60bf3dac
dariusl <d.lapunas@gmail.com>
2014-06-23 11:11:06 +0300

    upgrade stuff

--- invaders v2/UpgradeMenu.cpp
+++ invaders v2/UpgradeMenu.cpp
@@ -12,16 +12,34 @@ UpgradeMenu::UpgradeMenu(e::XMVECTOR pos, e::unordered_map<int, int> playerData)
 	or.push_back(UpgradeObservers::Register(UPGRADE_EVENT_WEPON_DMG_SET, e::bind(&UpgradeMenu::SetProperty, this, UPGRADE_EVENT_WEPON_DMG_SET, e::placeholders::_1)));
 	or.push_back(UpgradeObservers::Register(UPGRADE_EVENT_WEPON_PERIOD_SET, e::bind(&UpgradeMenu::SetProperty, this, UPGRADE_EVENT_WEPON_PERIOD_SET, e::placeholders::_1)));
 
-	Add(e::make_unique<CounterButton>(Utils::VectorSet(), "RESTORE HEALTH", 40.0f, [=](){this->Upgrade(UPGRADE_EVENT_HEALTH_SET); }, 100));
-	Add(e::make_unique<CounterButton>(Utils::VectorSet(), "MORE HEALTH", 40.0f, [=](){this->Upgrade(UPGRADE_EVENT_MAX_HEALTH_SET); }, 100));
-	Add(e::make_unique<CounterButton>(Utils::VectorSet(), "MORE DAMAGE", 40.0f, [=](){this->Upgrade(UPGRADE_EVENT_WEPON_DMG_SET); }, 100));
-	Add(e::make_unique<CounterButton>(Utils::VectorSet(), "FASTER GUN", 40.0f, [=](){this->Upgrade(UPGRADE_EVENT_WEPON_PERIOD_SET); }, 100));
+	Add(e::make_unique<CounterButton>(Utils::VectorSet(), "CURRENT HEALTH", 40.0f, [=](){this->Upgrade(UPGRADE_EVENT_HEALTH_SET); }, values[UPGRADE_EVENT_HEALTH_SET]));
+	Add(e::make_unique<CounterButton>(Utils::VectorSet(), "MAX HEALTH", 40.0f, [=](){this->Upgrade(UPGRADE_EVENT_MAX_HEALTH_SET); }, values[UPGRADE_EVENT_MAX_HEALTH_SET]));
+	Add(e::make_unique<CounterButton>(Utils::VectorSet(), "DAMAGE", 40.0f, [=](){this->Upgrade(UPGRADE_EVENT_WEPON_DMG_SET); }, values[UPGRADE_EVENT_WEPON_DMG_SET]));
+	Add(e::make_unique<CounterButton>(Utils::VectorSet(), "FIRE PERIOD", 40.0f, [=](){this->Upgrade(UPGRADE_EVENT_WEPON_PERIOD_SET); }, values[UPGRADE_EVENT_WEPON_PERIOD_SET]));
+
 	EnableUpgrades();
 }
 
 void UpgradeMenu::SetProperty(int key, int val)
 {
 	values[key] = val;
+	switch (key)
+	{
+	case UPGRADE_EVENT_HEALTH_SET:
+		static_cast<CounterButton&>(*items[0]).SetCount(val);
+		break;
+	case UPGRADE_EVENT_MAX_HEALTH_SET:
+		static_cast<CounterButton&>(*items[1]).SetCount(val);
+		break;
+	case UPGRADE_EVENT_WEPON_DMG_SET:
+		static_cast<CounterButton&>(*items[2]).SetCount(val);
+		break;
+	case UPGRADE_EVENT_WEPON_PERIOD_SET:
+		static_cast<CounterButton&>(*items[3]).SetCount(val);
+		break;
+	default:
+		break;
+	}
 }
 
 void UpgradeMenu::Upgrade(int key)
@@ -36,7 +54,7 @@ void UpgradeMenu::Upgrade(int key)
 			UpgradeObservers::Notify(UPGRADE_EVENT_EXP_SET, values[UPGRADE_EVENT_EXP_SET] - 100);
 			break;
 		case UPGRADE_EVENT_MAX_HEALTH_SET:
-			UpgradeObservers::Notify(UPGRADE_EVENT_MAX_HEALTH_SET, values[UPGRADE_EVENT_MAX_HEALTH_SET] + 20);
+			UpgradeObservers::Notify(UPGRADE_EVENT_MAX_HEALTH_SET, values[UPGRADE_EVENT_MAX_HEALTH_SET] + 100);
 			UpgradeObservers::Notify(UPGRADE_EVENT_EXP_SET, values[UPGRADE_EVENT_EXP_SET] - 100);
 			break;
 		case UPGRADE_EVENT_WEPON_DMG_SET:
